0b5278083fbf15fcd632113007889cd9698f5e84
Brendan Cullen <bcullen@uoregon.edu>
2021-02-18 10:43:46 -0800

    remove old comment

--- tests/testthat/test-get_survey_responses_raw.R
+++ tests/testthat/test-get_survey_responses_raw.R
@@ -3,14 +3,6 @@ test_that("Returns proper errors.", {
   expect_error(get_survey_responses_raw(session = 0, path_to_creds = ""), "A credentials file does not exist at that path.")
 })
 
-## How can I write unit tests that will access credentials from external file?
-# test_that("Output is a tibble", {
-#   cred_path <- "../credentials.csv"
-#   s0_data <- get_survey_responses_raw(session = 0, path_to_creds = cred_path)
-#   expect_true(tibble::is_tibble(s0_data))
-# })
-
-
 test_that('Credential file errors', {
   mockery::stub(get_survey_responses_raw, 'file.exists', TRUE)
   mockery::stub(get_survey_responses_raw, 'read_csv', tibble::tibble(invalid_key = "key"))
