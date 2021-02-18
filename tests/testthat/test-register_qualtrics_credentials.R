644a7553038f621e9852c8453f894c8a8f2eb179
Brendan Cullen <bcullen@uoregon.edu>
2021-02-18 15:27:01 -0800

    fix test for register_qualtrics_credentials

--- tests/testthat/test-register_qualtrics_credentials.R
+++ tests/testthat/test-register_qualtrics_credentials.R
@@ -1,9 +1,9 @@
 test_that("Returns proper errors.", {
-  expect_error(get_survey_responses_raw(session = 0, path_to_creds = ""), "A credentials file does not exist at that path.")
+  expect_error(register_qualtrics_credentials(path_to_creds = ""), "A credentials file does not exist at that path.")
 
   # stub out the function to avoid including real API credentials
-  mockery::stub(get_survey_responses_raw, 'file.exists', TRUE)
-  mockery::stub(get_survey_responses_raw, 'read_csv', tibble::tibble(invalid_key = "key"))
-  expect_error(get_survey_responses_raw(session = 0, path_to_creds = ""),
+  mockery::stub(register_qualtrics_credentials, 'file.exists', TRUE)
+  mockery::stub(register_qualtrics_credentials, 'read_csv', tibble::tibble(invalid_key = "key"))
+  expect_error(register_qualtrics_credentials(path_to_creds = ""),
                "Your credentials file must have the following columns: data_source, base_url, api_token")
 })
