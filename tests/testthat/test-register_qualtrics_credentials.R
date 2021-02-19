13db6548c168c9a446e323a5063cda364ba19213
Brendan Cullen <bcullen@uoregon.edu>
2021-02-18 16:36:30 -0800

    use fixed = TRUE and update code formatting

--- tests/testthat/test-register_qualtrics_credentials.R
+++ tests/testthat/test-register_qualtrics_credentials.R
@@ -1,9 +1,15 @@
 test_that("Returns proper errors.", {
-  expect_error(register_qualtrics_credentials(path_to_creds = ""), "A credentials file does not exist at that path.")
+  expect_error(
+    register_qualtrics_credentials(path_to_creds = ""),
+    "A credentials file does not exist at that path.",
+    fixed = TRUE
+  )
 
   # stub out the function to avoid including real API credentials
   mockery::stub(register_qualtrics_credentials, 'file.exists', TRUE)
   mockery::stub(register_qualtrics_credentials, 'read_csv', tibble::tibble(invalid_key = "key"))
-  expect_error(register_qualtrics_credentials(path_to_creds = ""),
-               "Your credentials file must have the following columns: data_source, base_url, api_token")
+  expect_error(
+    register_qualtrics_credentials(path_to_creds = ""),
+    "Your credentials file must have the following columns: data_source, base_url, api_token"
+  )
 })
