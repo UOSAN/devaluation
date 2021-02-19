d762e8404acb2e739198618453cb154592d35994
Brendan Cullen <bcullen@uoregon.edu>
2021-02-18 16:05:08 -0800

    add specific functions to import

--- R/devaluation-package.R
+++ R/devaluation-package.R
@@ -1,11 +1,11 @@
 #' @keywords internal
 #' @importFrom magrittr %>%
-#' @importFrom dplyr filter pull mutate
+#' @importFrom dplyr filter pull mutate arrange select
 #' @importFrom tibble as_tibble
 #' @importFrom stringr str_detect str_extract
 #' @importFrom rlang .data
 #' @importFrom readr read_csv
-#' @importFrom qualtRics qualtrics_api_credentials fetch_survey
+#' @importFrom qualtRics qualtrics_api_credentials fetch_survey all_surveys
 "_PACKAGE"
 
 # The following block is used by usethis to automatically manage
