5be6090ccd3058719764852f35d61244d70d0fb1
Brendan Cullen <bcullen@uoregon.edu>
2021-02-21 16:14:38 -0800

    refactor part of get_survey_data and update documentation

--- R/get_survey_data.R
+++ R/get_survey_data.R
@@ -1,20 +1,27 @@
 #' Get survey data
 #'
-#' @param surveys_df A tibble as returned by the \code{\link{get_survey_info}}.
-#' @param pid_col A character with the column name specifying user ID.
+#' @param surveys_df A tibble as returned by \code{\link{get_survey_info}}.
+#' @param pid_col A character with the column name specifying user ID or a regular expression
+#' identifying the column(s) that you'd like to keep in wide format. Defaults to a regular expression
+#' that selects columns that are useful for determining valid (e.g. non-duplicate) responses:
+#' * "Login" (participant ID)
+#' * "ResponseId" (alphanumeric sequence that uniquely identifies a set of survey
+#' responses for an individual)
+#' * "Finished" (indicates whether the survey was completed or not)
 #'
-#' @return Returns a long format tibble of survey data with names "SID variable name", "item", "value", "survey_name"
+#' @return Returns a long format tibble of survey data with columns identified by \code{pid_col}
+#' along with columns labeled "item", "value", and "survey_name"
 #' @export
-get_survey_data <- function(surveys_df, pid_col='Login'){
+get_survey_data <- function(surveys_df, pid_col = '(Login|ResponseId|Finished)'){
 
+  # get nested data frame with survey responses in list column
   survey_data <- surveys_df %>%
-    dplyr::group_by(name) %>%
-    dplyr::do(
-      survey_name = .$name[[1]],
-      survey_data = get_survey_responses(
-        survey_id = .$id[[1]])
-    )
+    select(survey_id = id, survey_name = name) %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(survey_data = list(get_survey_responses(survey_id = survey_id)))
 
+  # wrangle into long format
+  # BC: Struggling to refactor this...
   long_survey_data <- survey_data %>%
     dplyr::filter(dim(survey_data)[1]>0) %>%
     dplyr::do({
@@ -26,6 +33,7 @@ get_survey_data <- function(surveys_df, pid_col='Login'){
       aDF$survey_name <- .$survey_name
       aDF
     })
+
   return(long_survey_data)
 }
 
@@ -40,9 +48,9 @@ get_survey_data <- function(surveys_df, pid_col='Login'){
 #' @param force_request default is \code{TRUE}.
 #' @export
 get_survey_responses<-function(survey_id, label = FALSE, convert = FALSE, force_request = TRUE) {
-  responsesDF <- qualtRics::fetch_survey(survey_id,
+  responses_df <- qualtRics::fetch_survey(survey_id,
                                          label = label,
                                          convert = convert,
                                          force_request = force_request)
-  return(responsesDF)
+  return(responses_df)
 }
