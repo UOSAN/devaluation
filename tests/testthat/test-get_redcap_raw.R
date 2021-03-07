37c8a0f2e81a71ea3465a8370710e9d8732c72c9
Brendan Cullen <bcullen@uoregon.edu>
2021-03-07 11:05:50 -0800

    add more unit tests to get_redcap_raw()

--- tests/testthat/test-get_redcap_raw.R
+++ tests/testthat/test-get_redcap_raw.R
@@ -1,3 +1,19 @@
+test_that("Returns proper errors.", {
+  expect_error(
+    get_redcap_raw(path_to_creds = ""),
+    "A credentials file does not exist at that path.",
+    fixed = TRUE
+  )
+
+  # stub out the function to avoid including real API credentials
+  mockery::stub(get_redcap_raw, 'file.exists', TRUE)
+  mockery::stub(get_redcap_raw, 'read_csv', tibble::tibble(invalid_key = "key"))
+  expect_error(
+    get_redcap_raw(path_to_creds = ""),
+    "Your credentials file must have the following columns: data_source, base_url, api_token"
+  )
+})
+
 # list with extraneous rows
 fake_data <- list(data = tibble::tribble(
   ~dev_id,   ~redcap_event_name, ~first_name, ~last_name, ~email, ~phone, ~address, ~friend,
